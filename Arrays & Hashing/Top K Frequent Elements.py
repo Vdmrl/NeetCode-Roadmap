5d84f674e27dc94c91b9d8693fa3cb894df66aad
Vdmrl <vvluzin2004@gmail.com>
2023-07-24 17:37:55 +0500

    Top K Frequent Elements with DefaultDict

--- Arrays & Hashing/Top K Frequent Elements.py
+++ Arrays & Hashing/Top K Frequent Elements.py
@@ -1,9 +1,17 @@
 from typing import List
+from collections import defaultdict
 from collections import Counter
 
 
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    def topKFrequentDict(self, nums: List[int], k: int) -> List[int]:
+        dic = defaultdict(int)
+        for number in nums:
+            dic[number] += 1
+        dic = sorted(dic, key=dic.get, reverse=True)
+        return list(dic[0:k])
+
+    def topKFrequentCounter(self, nums: List[int], k: int) -> List[int]:
         count = Counter(nums).most_common(k)
         ans = []
         for i in count:
