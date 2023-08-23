de7a6d4816a971e0953caa8cec057996e2b582fe
Vdmrl <vvluzin2004@gmail.com>
2023-08-23 14:32:05 +0500

    Search in Rotated Sorted Array log(n) Solution without pivot searching

--- Binary Search/Search in Rotated Sorted Array.py
+++ Binary Search/Search in Rotated Sorted Array.py
@@ -25,52 +25,53 @@ class Solution:
             else:
                 return (mid - swap) % len(nums)
         return -1
+
     def searchLogN(self, nums: List[int], target: int) -> int:
         # 42ms 92%
         # 16.6mb 93%
         # log(n)
         # solution without pivot searching
-        left, right = 0, len(nums)-1
+        left, right = 0, len(nums) - 1
         while left <= right:
             mid = (left + right) // 2
             if target > nums[mid]:
-                if nums[left] > nums[mid] and target > nums[right]: # left part
+                if nums[left] > nums[mid] and target > nums[right]:  # left part
                     right = mid - 1
-                else: # right part
+                else:  # right part
                     left = mid + 1
             elif target < nums[mid]:
-                if nums[left] <= nums[mid] and target < nums[left]: # left part
+                if nums[left] <= nums[mid] and target < nums[left]:  # left part
                     left = mid + 1
-                else: # right part
-                    right = mid - 1
-            else:
-                return mid
-        return -1
-    def searchLogNUnd(self, nums: List[int], target: int) -> int:
-        # 51ms 52%
-        # 16.6mb 70%
-        # log(n)
-        # solution without pivot searching  (more understandable solution)
-        left, right = 0, len(nums)-1
-        while left <= right:
-            mid = (left + right) // 2
-            if target > nums[mid]:
-                if nums[left] <= nums[mid]: # left part
-                    left = mid + 1
-                else: # right part
-                    if target <= nums[right]:
-                        left = mid + 1
-                    else:
-                        right = mid - 1
-            elif target < nums[mid]:
-                if nums[left] <= nums[mid]: # left part
-                    if target >= nums[left]:
-                        right = mid - 1
-                    else:
-                        left = mid + 1
-                else: # right part
+                else:  # right part
                     right = mid - 1
             else:
                 return mid
         return -1
 
+    def searchLogNUnd(self, nums: List[int], target: int) -> int:
+        # 51ms 52%
+        # 16.6mb 70%
+        # log(n)
+        # solution without pivot searching  (more understandable solution)
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if target > nums[mid]:
+                if nums[left] <= nums[mid]:  # left part
+                    left = mid + 1
+                else:  # right part
+                    if target <= nums[right]:
+                        left = mid + 1
+                    else:
+                        right = mid - 1
+            elif target < nums[mid]:
+                if nums[left] <= nums[mid]:  # left part
+                    if target >= nums[left]:
+                        right = mid - 1
+                    else:
+                        left = mid + 1
+                else:  # right part
+                    right = mid - 1
+            else:
+                return mid
+        return -1
