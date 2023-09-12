525a348cfaa10cc24c77cd7294d308baaab81c25
Vdmrl <vvluzin2004@gmail.com>
2023-09-13 00:00:40 +0500

    Find the Duplicate Number Fixed

--- Linked List/Find the Duplicate Number.py
+++ Linked List/Find the Duplicate Number.py
@@ -3,10 +3,16 @@ from typing import List
 
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        slow, fast = 0, nums[0]
-
-        while slow != fast:
+        # 532ms - 52%
+        # 30.4mb - 98%
+        slow, fast = 0, 0
+        while True:
             slow, fast = nums[slow], nums[nums[fast]]
-
-
+            if slow == fast:
+                break
+        slow_new = 0
+        while slow != slow_new:
+            slow, slow_new = nums[slow], nums[slow_new]
+            if slow == slow_new:
+                break
         return slow
