f28de8717d7e791045622227bfc34e3b63f18d7c
Vdmrl <vvluzin2004@gmail.com>
2023-09-17 01:19:02 +0500

    Maximum Depth of Binary Tree Iterable solution

--- Trees/Maximum Depth of Binary Tree.py
+++ Trees/Maximum Depth of Binary Tree.py
@@ -1,14 +1,32 @@
 from typing import Optional
+from collections import deque
+
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+
+
 class Solution:
     def maxDepthRec(self, root: Optional[TreeNode]) -> int:
         # O(n) - 41ms - 88%
         # O(n) - 18.6mb - 76%
         if not root:
             return 0
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        return 1 + max(self.maxDepthRec(root.left), self.maxDepthRec(root.right))
+
+    def maxDepthIter(self, root: Optional[TreeNode]) -> int:
+        # O(n) - 45ms - 70%
+        # O(n) - 18.6mb - 90%
+        ans = 0
+        dq = deque([(root, 1)])
+        while dq:
+            node, depth = dq.popleft()
+            if node:
+                ans = max(ans, depth)
+                dq.append((node.left, depth + 1))
+                dq.append((node.right, depth + 1))
+        return ans
